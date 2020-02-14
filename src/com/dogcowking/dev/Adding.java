87125e6c68123e5ddede41a1a06f603ace40eda4
jshasus <jshasus@DESKTOP-EFU1L44>
2020-02-14 16:32:55 +0900

    adding camelToString

--- src/com/dogcowking/dev/Adding.java
+++ src/com/dogcowking/dev/Adding.java
@@ -1,7 +1,19 @@
 package com.dogcowking.dev;
 
 import java.lang.reflect.Field;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.junit.Test;
+
+/** 
+ * 배열
+ * 리스트
+ * 오브젝트 테스트 해봐야 super 도
+ * 
+ * @author jshasus
+ *
+ */
 public class Adding {
 
 	/**
@@ -12,24 +24,70 @@ public class Adding {
 	public static String toString(Object o) {
 		String r = "";
 		Class c = o.getClass();
-		System.out.println("클래스 : " + c);
+		
+		r+=c.getName()+"(";
 		int i=0;
 		for(Field f : c.getDeclaredFields() ) {
-			System.out.println("필드 " + i + " " + f);
+			
+			f.setAccessible(true);
+			
 			try {
-				r += f.getName()+":"+f.get(o);
+				r += f.getName()+"="+f.get(o);
 			} catch (IllegalArgumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				throw new RuntimeException(e);
 			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				throw new RuntimeException(e);
 			}
 			
-			r += "/";
+			f.setAccessible(false);
+			
+			r += ", ";
 		}
+		r+=")";
 		
 		return r;
+	}
+	
+	
+	
+	/**
+	 * 
+	 */
+	public static String snakeToCamel(String s) {
+		StringBuilder sb = new StringBuilder();
+		
+		Pattern p = Pattern.compile("_([a-z])");
+		Matcher m = p.matcher(s);
+		
+		int last=0;
+		while(m.find()) { 
+			sb.append(s.substring(last, m.start()));
+			sb.append(m.group(1).toUpperCase());
+			last = m.end();
+		}
+		sb.append(s.substring(last));
+		
+		
+		return sb.toString();
+	}
+	
+	
+	@Test
+	public void test_snakeToCamel() { 
+		String s="mst_data_id,\r\n" + 
+				"			mst_data_nm,\r\n" + 
+				"			mst_data_tbl_nm,\r\n" + 
+				"			mst_data_tbl_id,\r\n" + 
+				"			mst_data_desc,\r\n" + 
+				"			data_tp_cd,\r\n" + 
+				"			data_dtl_tp_cd,\r\n" + 
+				"			sub_sys_cd,\r\n" + 
+				"			mngr_eno,\r\n" + 
+				"			data_bank_path,\r\n" + 
+				"			data_bank_file_nm,\r\n" + 
+				"			data_bank_tbl_nm,\r\n" + 
+				"			rltn_rcv_data_id";
+		System.out.println(snakeToCamel(s));
 	}
 	
 	public static class A {
@@ -44,5 +102,14 @@ public class Adding {
 		r = toString(a);
 		
 		System.out.println(r);
+		
+		
+		TestA testA = new TestA();
+		r = toString(testA);
+		System.out.println(r);
+		
+		System.out.println("c: "  + testA.c);
+		
+		
 	}
 }
---
+++ src/com/dogcowking/dev/TestA.java
@@ -0,0 +1,8 @@
+package com.dogcowking.dev;
+
+public class TestA {
+
+	private int a;
+	private long b;
+	public long c;
+}
